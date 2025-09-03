54db3df346902dd10b3808cade4c346f934acf93
smujaawar <smujaawar@gmail.com>
2025-09-03 19:04:32 +0530

    React first commit react intro features jsx class components vs functional components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ClassComp from "./components/ClassComp";
+import JsxDemo from "./components/JsxDemo";
+
+function App() {
+  return (
+    <>
+      <h1>Hello premium batch</h1>
+      <JsxDemo />
+      <ClassComp />
+    </>
+  );
+}
+
+export default App;
